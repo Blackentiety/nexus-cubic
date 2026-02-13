4f624224943791e200136b3a13cdf23f497795fd
Blackentiety <lukas.boudinl@gmail.com>
2026-02-13 14:59:46 +0100

    class faction

---
+++ src/Entity/Faction.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FactionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FactionRepository::class)]
+class Faction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $power = null;
+
+    #[ORM\Column]
+    private ?int $idCreator = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPower(): ?int
+    {
+        return $this->power;
+    }
+
+    public function setPower(int $power): static
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    public function getIdCreator(): ?int
+    {
+        return $this->idCreator;
+    }
+
+    public function setIdCreator(int $idCreator): static
+    {
+        $this->idCreator = $idCreator;
+
+        return $this;
+    }
+}
