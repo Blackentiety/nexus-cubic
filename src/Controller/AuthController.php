19ec6263efb820de235ed6ab0753b5ac8755f59f
Blackentiety <lukas.boudinl@gmail.com>
2026-02-13 13:02:30 +0100

    auth.register

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AuthController extends AbstractController
+{
+    public function __construct(private UserRepository $userRepository){}
+
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function registerUser(Request $request, EntityManagerInterface $em): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        if (!$data){
+            return $this->json([
+                'status' => 'error',
+                'message' => 'Invalid JSON'
+            ]);
+        }
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setPassword(md5($data['password']));
+        $user->setRole('ROLE_USER');
+        $user->setPseudoMinecraft($data['pseudoMinecraft']);
+        $user->setUuidMinecraft($data['uuidMinecraft']);
+        $user->setCredits(0.0);
+        $user->setDateInscription(new \DateTime());
+        $token = md5(uniqid());
+        $user->setToken($token);
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([
+            'status' => 'success',
+            'token' => $token,
+            'userId' => $user->getId(),
+            'PseudoMinecraft' => $user->getPseudoMinecraft()
+        ]);
+    }
+    #[Route('/auth', name: 'app_auth')]
+    public function index(): Response
+    {
+        return $this->render('auth/index.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+}
